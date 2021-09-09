ff61acd156f80e204f568f69e34254d31c41d6b9
samat75 <busher@01>
2021-09-09 19:09:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/geegfoot/GeekSplash.kt
@@ -0,0 +1,24 @@
+package com.example.geegfoot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class GeekSplash : Fragment() {
+
+    companion object {
+        fun newInstance() = GeekSplash()
+    }
+
+    private lateinit var viewModel: GeekSplashViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.geek_splash_fragment, container, false)
+    }
+
+}
